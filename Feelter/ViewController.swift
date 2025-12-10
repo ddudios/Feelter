214c247f36b26f297c821bd0ea34bc5666627f38
ddudi <ddudios@naver.com>
2025-12-10 17:25:13 +0900

    .gitignore 설정

---
+++ Feelter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Feelter
+//
+//  Created by Suji Jang on 12/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
